b2d2d73ddbbca1521877fc4281d0c4a88e109a01
USUARIO <USUARIO@pc1-210>
2018-09-19 20:54:46 -0500

    se borraron datos y se volvieron a crear

---
+++ src/conexionmysql/corba/ConexionMySQLCorba.java
@@ -0,0 +1,12 @@
+package conexionmysql.corba;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ConexionMySQLCorba {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
